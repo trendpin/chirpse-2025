0983a60372b201a82df3015c0e135270dedb9315
trendpin <theoopsgallery@gmail.com>
2025-11-21 01:59:54 +0000

    Update app.js Fixed

--- app.js
+++ app.js
@@ -2,12 +2,10 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebas
 import { getAuth, onAuthStateChanged, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 import { getFirestore, doc, setDoc, getDoc, collection, onSnapshot, query, orderBy, addDoc, serverTimestamp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 import { getStorage, ref, uploadBytes, getDownloadURL } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-storage.js";
-
-// THIS IS THE ONLY CONFIG THAT ACTUALLY WORKS ON GITHUB PAGES IN 2025
 const firebaseConfig = {
   apiKey: "AIzaSyDniXwAIq7jCu3RkZBUnt76tgW5uC1Ld8",
   authDomain: "chirpse-2025.firebaseapp.com",
   projectId: "chirpse-2025",
-  storageBucket: "chirpse-2025.appspot.com",           // ← THIS LINE FIXES EVERYTHING
+  storageBucket: "chirpse-2025.appspot.com",          
   messagingSenderId: "895121502746",
   appId: "1:895121502746:web
